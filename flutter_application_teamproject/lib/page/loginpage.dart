97474b73192be8e11aab98a15e062dd19bb7b970
peemOpas <74456372+peemOpas@users.noreply.github.com>
2022-04-07 00:04:33 +0700

    change login ui

--- flutter_application_teamproject/lib/page/loginpage.dart
+++ flutter_application_teamproject/lib/page/loginpage.dart
@@ -13,18 +13,15 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
-
   final formkey = GlobalKey<FormState>();
   Profile profile =Profile(email: '',password: '');
   final Future<FirebaseApp> firebase =Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-   return FutureBuilder(
+   return  FutureBuilder(
       future:firebase,
-      builder: (context,snapshot)
+      builder:(context,snapshot)
       {
-
-
         if(snapshot.hasError){
           return Scaffold(
             appBar:AppBar(
@@ -35,84 +32,123 @@ class _LoginPageState extends State<LoginPage> {
             ),
           );
         }
-
-
         if(snapshot.connectionState == ConnectionState.done){
            return Scaffold(
-                  appBar: AppBar(title: Text("LoginPage"),),
-                  body:Container(
-                  child: Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: Form(
-                  key: formkey,
-                  child:SingleChildScrollView(
-                  child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text("Email",style:TextStyle(fontSize: 20)),
-                TextFormField(
-                  validator: MultiValidator([
-                    RequiredValidator(errorText: "Pls fill the form"),
-                    EmailValidator(errorText: "Pls fill the correct email form"),
-                    ]         
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  onSaved:(String? email){
-                    profile.email =email!;
-                  },
+                  appBar: AppBar(title: Text("LoginPage"), 
+                  backgroundColor: Colors.black,),
+
+
+
+                  body:Padding(  
+                    padding: const EdgeInsets.all(0),     
+                    child: Container(  
+                    height: 1000,                
+                    decoration: BoxDecoration(
+                    image:  DecorationImage(image: AssetImage("assets/images/bg2login.png"),
+                    fit: BoxFit.cover)
+          //  borderRadius: BorderRadius.circular(20),
+          //  color: Colors.blue[100] ,
+          //  border: Border.all(
+          //    color: Colors.blue,
+          //    width:12,
+          //  )
+           
+          ),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Form(
+                        key: formkey,
+                        child:SingleChildScrollView(
+                          child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [  SizedBox(height: 10,),
+                          Image.asset("assets/images/pocketphamaw.png"),
+                          SizedBox(height: 20,),   
+                          Text("Email",style:TextStyle(fontSize: 20)),
+                          TextFormField(
+                        validator: MultiValidator([
+                          RequiredValidator(errorText: "Pls fill the form"),
+                          EmailValidator(errorText: "Pls fill the correct email form"),
+                          ]         
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onSaved:(String? email){
+                          profile.email =email!;
+                        },
                 ),
                 SizedBox(height: 15,),
                 Text("Password",style:TextStyle(fontSize: 20)),
                 TextFormField(
-                  validator: RequiredValidator(errorText: "Pls fill the correct password"),
-                  obscureText: true,
-                   onSaved:(String? password){
-                    profile.password = password!;
-                  },
+                        validator: RequiredValidator(errorText: "Pls fill the correct password"),
+                        obscureText: true,
+                         onSaved:(String? password){
+                          profile.password = password!;
+                        },
                 ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    child: Text("Login"),
-                    onPressed: () async{
-                        if(formkey.currentState!.validate()){
-                            formkey.currentState?.save();
-                          try{
-                             await FirebaseAuth.instance.signInWithEmailAndPassword(
-                               email: profile.email, 
-                               password: profile.password
-                               ).then((value){
-                                 formkey.currentState?.reset();
-                                  Navigator.pushReplacement(
-                                  context,MaterialPageRoute(builder: (context){
-                                  return WelcomePage();
-                            })
-                            );
-
-                               });
-                          
-                          }on FirebaseAuthException catch(e){
-                            //  print(e.message);
-                            //  print(e.code);
-                              Fluttertoast.showToast(
-                              msg: e.message!,
-                              gravity: ToastGravity.CENTER
-                            );
-                          }
-                        }   
-                    },)
-                  )
-            ],),
-          )),
-        )
-      ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: SizedBox(
+                            width: 120,
+                            height: 50,
+                            child: ElevatedButton(
+                              child: Text("Sign in"),
+                        
+                              style: ElevatedButton.styleFrom(
+                            primary: Colors.black,
+                            shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0), 
+                            side: BorderSide(color: Colors.black87)                          
+  )
+),  
+                              
+                              onPressed: () async{
+                                  if(formkey.currentState!.validate()){
+                                      formkey.currentState?.save();
+                                    try{
+                                       await FirebaseAuth.instance.signInWithEmailAndPassword(
+                                         email: profile.email, 
+                                         password: profile.password
+                                         ).then((value){
+                                           formkey.currentState?.reset();
+                                            Navigator.pushReplacement(
+                                            context,MaterialPageRoute(builder: (context){
+                                            return WelcomePage();
+                                      })
+                                      );
+                                         });
+                                    
+                                    }on FirebaseAuthException catch(e){
+                                      //  print(e.message);
+                                      //  print(e.code);
+                                        Fluttertoast.showToast(
+                                        msg: e.message!,
+                                        gravity: ToastGravity.CENTER
+                                      );
+                                    }
+                                  }   
+                              },)
+                            ),
+                  ),
+                ),
+                Text("Don't have an account?"),
+            ],
+            ),
+          )
+          ),
+                      ),
+                    ),
+                      ),
+                   
+                  ),
     );
         }
-
-
         return Scaffold(
-          body:Center(
+          body:Container(
+            
             child: CircularProgressIndicator(),
+            
             ),
         );
     });
