0c980e34555597d1377293d0f39a7b079c64b3ab
teelekxx <kittipon.ithub@gmail.com>
2022-04-28 00:37:19 +0700

    [ADD] home page

--- flutter_application_teamproject/lib/page/welcomepage.dart
+++ flutter_application_teamproject/lib/page/welcomepage.dart
@@ -1,10 +1,8 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_teamproject/data/menu_items.dart';
 import 'package:flutter_application_teamproject/model/meun_item.dart';
 import 'package:flutter_application_teamproject/page/chatpage.dart';
-import 'package:flutter_application_teamproject/page/home.dart';
 import 'package:flutter_application_teamproject/page/homepage.dart';
 import 'package:flutter_application_teamproject/page/mappage.dart';
 import 'package:flutter_application_teamproject/page/profilepage.dart';
@@ -17,124 +15,126 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage> {
   final auth = FirebaseAuth.instance;
-  int index =0;
+  int index = 0;
 
-  final screen =[
+  final screen = [
     MainPage(),
-    MapPage(),  
+    MapPage(),
     ChatPage(),
     ProfilePage(),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text("Welcompage"),
-      
-      actions: [ 
-        // IconButton(icon: Icon(Icons.search),onPressed: (){
-        //   showSearch(context: context, delegate: MySearchDelegate());
-        // }) ,
-        PopupMenuButton<MenuItem>(
-          onSelected: (item)=>onSelected(context,item),
-          itemBuilder: (context)=>[
-           ...MenuItems.itemFirst.map(buildItem).toList(),  
-           PopupMenuDivider(),
-           ...MenuItems.itemSecond.map(buildItem).toList(),  
-        ],     
-        )
-      ],
-      backgroundColor: Colors.black,
-      ),
-      body:screen[index],
-      bottomNavigationBar:
-      NavigationBarTheme(
-        data: NavigationBarThemeData(
-          indicatorColor: Colors.white,
-        
-          labelTextStyle: MaterialStateProperty.all(
-            TextStyle(fontSize: 14,fontWeight:FontWeight.w500,color: Colors.white)
-            ),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Welcompage"),
+          actions: [
+            // IconButton(icon: Icon(Icons.search),onPressed: (){
+            //   showSearch(context: context, delegate: MySearchDelegate());
+            // }) ,
+            PopupMenuButton<MenuItem>(
+              onSelected: (item) => onSelected(context, item),
+              itemBuilder: (context) => [
+                ...MenuItems.itemFirst.map(buildItem).toList(),
+                PopupMenuDivider(),
+                ...MenuItems.itemSecond.map(buildItem).toList(),
+              ],
+            )
+          ],
+          backgroundColor: Colors.black,
+        ),
+        body: screen[index],
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+            indicatorColor: Colors.white,
+            labelTextStyle: MaterialStateProperty.all(TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Colors.white)),
           ),
-        child: navigatorbar(),)
+          child: navigatorbar(),
+        ));
+  }
 
+  NavigationBar navigatorbar() {
+    return NavigationBar(
+      backgroundColor: Colors.black,
+      selectedIndex: index,
+      labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+      animationDuration: Duration(seconds: 1),
+      onDestinationSelected: (index) => setState(() => this.index = index),
+      destinations: [
+        NavigationDestination(
+          icon: Icon(
+            Icons.home_outlined,
+            color: Colors.white,
+          ),
+          selectedIcon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.map_outlined, color: Colors.white),
+          selectedIcon: Icon(Icons.map),
+          label: 'Map',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.chat_outlined, color: Colors.white),
+          selectedIcon: Icon(Icons.chat),
+          label: 'Chat',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person_outline, color: Colors.white),
+          selectedIcon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
     );
   }
 
-NavigationBar navigatorbar(){
-  return NavigationBar(
-    backgroundColor: Colors.black,
-    selectedIndex: index,
-    labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-    animationDuration: Duration(seconds:1),
-    onDestinationSelected: (index)=>setState(() 
-          =>this.index =index),
-    destinations: [
-      NavigationDestination( 
-            icon: Icon(Icons.home_outlined,color: Colors.white,),
-            selectedIcon:Icon(Icons.home) ,
-            label:'Home',
-            ),
-      NavigationDestination( 
-            icon: Icon(Icons.map_outlined,color: Colors.white),
-            selectedIcon:Icon(Icons.map),
-            label:'Map',
-            ),
-      NavigationDestination( 
-            icon: Icon(Icons.chat_outlined,color: Colors.white),
-            selectedIcon:Icon(Icons.chat),
-            label:'Chat',
-            ),
-      NavigationDestination( 
-            icon: Icon(Icons.person_outline,color: Colors.white),
-            selectedIcon:Icon(Icons.person),
-            label:'Profile',
-            ),    
-    ],);
-}
-  PopupMenuItem<MenuItem> buildItem(MenuItem item) => 
-  PopupMenuItem<MenuItem>(
-    value:item,
-    child: Row(children: [
-    Icon(item.icon,color:Colors.yellow,size:20),
-    const SizedBox(width:12),
-    Text(item.text)
-    ],)
-    );
+  PopupMenuItem<MenuItem> buildItem(MenuItem item) => PopupMenuItem<MenuItem>(
+      value: item,
+      child: Row(
+        children: [
+          Icon(item.icon, color: Colors.yellow, size: 20),
+          const SizedBox(width: 12),
+          Text(item.text)
+        ],
+      ));
 
-  void onSelected(BuildContext context,MenuItem item){
-    switch (item){
+  void onSelected(BuildContext context, MenuItem item) {
+    switch (item) {
       case MenuItems.itemSetting:
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context)=> ProfilePage())
-          );
-          break;
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => ProfilePage()));
+        break;
       case MenuItems.itemSignout:
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context)=> HomePage())
-          );
-          break;
-      
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => HomePage()));
+        break;
     }
   }
 }
-class MySearchDelegate extends SearchDelegate{
+
+class MySearchDelegate extends SearchDelegate {
   @override
-  List<Widget> buildActions(BuildContext context) => [IconButton(icon: Icon(Icons.clear), 
-  onPressed:(){
-       if(query.isEmpty){
-      close(context, null);
-    }
-    else{
-      query='';
-    }
-  }
-  )];
+  List<Widget> buildActions(BuildContext context) => [
+        IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () {
+              if (query.isEmpty) {
+                close(context, null);
+              } else {
+                query = '';
+              }
+            })
+      ];
   @override
-  Widget buildLeading(BuildContext context) => IconButton(icon: Icon(Icons.arrow_back), 
-  onPressed:(){
-      close(context, null); 
-  });
+  Widget buildLeading(BuildContext context) => IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      });
   @override
   Widget buildSuggestions(BuildContext context) => Text("Suggestions go here");
   @override
