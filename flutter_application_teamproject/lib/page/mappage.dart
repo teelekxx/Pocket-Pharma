c0af990d89843bf95209fda06639394b4ae3f4be
peemOpas <74456372+peemOpas@users.noreply.github.com>
2022-04-28 19:57:02 +0700

    ..

--- flutter_application_teamproject/lib/page/mappage.dart
+++ flutter_application_teamproject/lib/page/mappage.dart
@@ -48,6 +48,19 @@ class _MapPageState extends State<MapPage> {
     print("Hi lat =${lat}, lng =${lng}");
   }
 
+  //   double calculateDistance(double lat1, double lng1, double lat2, double lng2) {
+//     double distance = 0;
+
+//     var p = 0.017453292519943295;
+//     var c = cos;
+//     var a = 0.5 -
+//         c((lat2 - lat1) * p) / 2 +
+//         c(lat1 * p) * c(lat2 * p) * (1 - c((lng2 - lng1) * p)) / 2;
+//     distance = 12742 * asin(sqrt(a));
+
+//     return distance;
+//   }
+
   Future<LocationData?> findLocationData() async{
     Location location =Location();
     try{
