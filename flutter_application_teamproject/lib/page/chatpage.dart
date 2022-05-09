f253fb6e73c5d028e92324a7dc4c7b8d31ab476d
peemOpas <74456372+peemOpas@users.noreply.github.com>
2022-05-09 19:05:12 +0700

    [ADD] modify chatpage

--- flutter_application_teamproject/lib/page/chatpage.dart
+++ flutter_application_teamproject/lib/page/chatpage.dart
@@ -30,13 +30,19 @@ navigateTodetail(DocumentSnapshot post){
 RoundedRectangleBorder myRoundedborder() {
     return RoundedRectangleBorder(
          borderRadius:BorderRadius.circular(10),
-        side: BorderSide(color: Colors.black));
+        side: BorderSide(color: Colors.black,width: 5));
   }
 
 RoundedRectangleBorder myRoundedborderpending() {
     return RoundedRectangleBorder(
          borderRadius:BorderRadius.circular(10),
-        side: BorderSide(color: Colors.red));
+        side: BorderSide(color: Colors.yellow.shade600,width: 5));
+  }
+
+RoundedRectangleBorder myRoundedborderaccept() {
+    return RoundedRectangleBorder(
+         borderRadius:BorderRadius.circular(10),
+        side: BorderSide(color: Colors.green,width: 5));
   }
 
 CircleAvatar myCircleAvatar(){
@@ -50,7 +56,16 @@ CircleAvatar myCircleAvatar(){
 
 CircleAvatar myCircleAvatarpending(){
   return CircleAvatar(
-          backgroundColor:Colors.red,
+          backgroundColor:Colors.yellow,
+          radius:30,
+          child:FittedBox(child: Icon(Icons.calendar_month,color: Colors.black,)
+            ,)
+    );
+}
+
+CircleAvatar myCircleAvataraccept(){
+  return CircleAvatar(
+          backgroundColor:Colors.green,
           radius:30,
           child:FittedBox(child: Icon(Icons.calendar_month,color: Colors.black,)
             ,)
@@ -133,32 +148,50 @@ CircleAvatar myCircleAvatarpending(){
               Timestamp t = snapshot.data[index].data()['created'];
               DateTime d =t.toDate();
               String request = snapshot.data[index].data()['status'];
-              if(snapshot.data[index].data()["owner"]==userID){           
+              if(snapshot.data[index].data()["owner"]==userID){ 
+                print(request);    
                   return Padding(
                   padding: const EdgeInsets.all(10.0),
-                  child: ListTile(
-                    focusColor: Colors.black,
-                     shape:request=="pending"?
-                       myRoundedborderpending()
-                      :myRoundedborder()
-                     ,
-                      leading:request=="pending"?
-                       myCircleAvatarpending()
-                      :myCircleAvatar(),
-                    title:Text(snapshot.data[index].data()["doctorName"]),      
-                    // title: RichText(text: TextSpan(children:[
-                    
-                    //   // WidgetSpan(child: Icon(Icons.people)),
-                    //   // TextSpan(text:"hi"+snapshot.data[index].data()["ownerName"])
-                    // ]),
-                    // ),
-                    subtitle: Text(d.toString()),
-                    contentPadding: EdgeInsets.symmetric(vertical:10,horizontal: 10),
-                    trailing:  Wrap(spacing: 12,
-                    children: <Widget>[              
-                            Icon(Icons.pending)
-                          ],),
-                    onTap: ()=> navigateTodetail(snapshot.data[index]),
+                  child: Ink(
+                    color:(request=="pending")?
+                    Colors.yellow[100]:
+                    (request=="Accept")?
+                    Colors.green[100]
+                    :Colors.black26,
+                      child: ListTile(
+                      focusColor: Colors.black,
+                       shape:(request=="pending")?
+                        myRoundedborderpending():
+                        (request=="Accept")?
+                        myRoundedborderaccept()
+                        :myRoundedborder()
+                       ,
+                        leading:(request=="pending")?
+                         myCircleAvatarpending():
+                        (request=="Accept")?
+                        myCircleAvataraccept()
+                        :myCircleAvatar(),
+                      title:Text(snapshot.data[index].data()["doctorName"]+" wow"),      
+                      // title: RichText(text: TextSpan(children:[
+                      
+                      //   // WidgetSpan(child: Icon(Icons.people)),
+                      //   // TextSpan(text:"hi"+snapshot.data[index].data()["ownerName"])
+                      // ]),
+                      // ),
+                      subtitle: Text(d.toString()),
+                      contentPadding: EdgeInsets.symmetric(vertical:10,horizontal: 10),
+                      trailing:  Wrap(spacing: 12,
+                      children: <Widget>[ 
+                              if(request=="pending")      
+                                Icon(Icons.alarm_add,size: 40,color: Colors.yellow.shade700,), 
+                                // Text("Pending...",style: TextStyle(fontSize: 20),),         
+                              if(request=="Accept")
+                                Icon(Icons.people_alt,size: 40,color: Colors.green.shade700),
+                              if(request=="")
+                                Icon(Icons.unsubscribe,size: 40,color: Colors.black87)
+                            ],),
+                      onTap: ()=> navigateTodetail(snapshot.data[index]),
+                    ),
                   ),
                 );     
                 // return Center(child: Text("Don't have any information"),);
--- flutter_application_teamproject/lib/page/homepage.dart
+++ flutter_application_teamproject/lib/page/homepage.dart
@@ -2,9 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_application_teamproject/page/startpage.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_application_teamproject/page/startpage.dart';
+// import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -15,113 +14,13 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   final auth = FirebaseAuth.instance;
   final uid = FirebaseAuth.instance.currentUser?.uid;
-  final name = FirebaseAuth.instance.currentUser?.displayName;
   // final uid = FirebaseAuth.instance.currentUser?.uid;
 
   final Future<FirebaseApp> firebase = Firebase.initializeApp();
-  DateTime selectedDate = DateTime.now();
-  TimeOfDay selectedTime = TimeOfDay.now();
-  String selectedDoctor = "";
-  String selectedClinic = "";
-  String selectedPhone = "";
-  var textController = TextEditingController();
-
-  DateTime join(DateTime date, TimeOfDay time) {
-    return new DateTime(
-        date.year, date.month, date.day, time.hour, time.minute);
-  }
-
-  Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? timeOfDay = await showTimePicker(
-      context: context,
-      initialTime: selectedTime,
-      initialEntryMode: TimePickerEntryMode.dial,
-    );
-    if (timeOfDay != null && timeOfDay != selectedTime) {
-      setState(() {
-        selectedTime = timeOfDay;
-        selectedDate = join(selectedDate, selectedTime);
-        FirebaseFirestore.instance.collection('appointment').add({
-          'created': selectedDate,
-          'owner': uid,
-          'symtom': textController.text,
-          'doctorName': selectedDoctor,
-          'clinicName': selectedClinic,
-          'phone': selectedPhone,
-        });
-        Fluttertoast.showToast(
-            msg: "Appointed!",
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.CENTER,
-            timeInSecForIosWeb: 1,
-            textColor: Colors.white,
-            fontSize: 16.0);
-        Navigator.of(context, rootNavigator: true).pop();
-      });
-    }
-  }
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime.now(),
-        lastDate: DateTime(2101));
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-        _selectTime(context);
-      });
-    }
-  }
-
-  Future<void> _appointment(BuildContext context) async {
-    final String? des = await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-              insetPadding: const EdgeInsets.all(10.0),
-              content: Container(
-                  width: double.maxFinite,
-                  child: ListView(children: [
-                    Text("Please tell us about yourself",
-                        style: TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.bold)),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      controller: textController,
-                      decoration: InputDecoration(
-                        hintText: 'Type here....',
-                        border: OutlineInputBorder(),
-                      ),
-                      maxLines: 20,
-                      minLines: 1,
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    SizedBox(
-                        width: 100,
-                        height: 50,
-                        child: ElevatedButton(
-                            child: Text("Next"),
-                            style: ElevatedButton.styleFrom(
-                                primary: Colors.black,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.black87))),
-                            onPressed: () {
-                              _selectDate(context);
-                            }))
-                  ])));
-        });
-  }
 
   @override
   Widget build(BuildContext context) {
-    print(name);
+    print(uid);
     return Padding(
         padding: const EdgeInsets.all(0.0),
         child: Center(
@@ -160,44 +59,86 @@ class _MainPageState extends State<MainPage> {
                               child: Material(
                                   child: InkWell(
                                 onTap: () {
-                                  textController.text = "";
                                   showDialog(
                                       context: context,
                                       builder: (BuildContext context) {
-                                        DateTime showDate = selectedDate;
+                                        List<String> _locations = [
+                                          '9:00 - 10:00',
+                                          '11:00 - 12:00',
+                                          '13:00 - 14:00',
+                                          '15:00 - 16:00',
+                                          '17:00 - 18:00'
+                                        ];
+                                        var available =
+                                            document["availability"];
+                                        var index = 0;
+                                        for (bool i in available) {
+                                          if (i == false) {
+                                            _locations[index] = "none";
+                                          }
+                                          index += 1;
+                                        }
+                                        List<String> filtered = _locations
+                                            .where((item) => item != "none")
+                                            .toList();
+                                        String? _dropDownValue = filtered[0];
                                         return AlertDialog(
                                           insetPadding:
                                               const EdgeInsets.symmetric(
                                                   vertical: 200,
                                                   horizontal: 50),
-                                          content: Column(
+                                          content: Stack(
                                             children: <Widget>[
                                               Align(
                                                 alignment: Alignment.topLeft,
                                                 child: ListTile(
-                                                  title: Text(
-                                                      document["clinicName"],
+                                                  title: Text(document["name"],
                                                       style: TextStyle(
                                                           fontSize: 25,
                                                           fontWeight:
                                                               FontWeight.bold)),
-                                                  subtitle: Text(
-                                                      document["doctorName"] +
-                                                          ",  " +
-                                                          document["type"]),
+                                                  subtitle:
+                                                      Text(document["type"]),
                                                 ),
                                               ),
                                               Align(
                                                   alignment: Alignment.center,
                                                   child: Text(document["des"])),
-                                              SizedBox(
-                                                height: 40,
-                                              ),
                                               Align(
-                                                alignment:
-                                                    Alignment.bottomCenter,
+                                                  alignment:
+                                                      Alignment.bottomRight,
+                                                  child: Container(
+                                                      width: 100,
+                                                      child:
+                                                          DropdownButtonFormField(
+                                                        value: _dropDownValue,
+                                                        isExpanded: true,
+                                                        iconSize: 30.0,
+                                                        style: TextStyle(
+                                                            color: Colors.blue),
+                                                        items: filtered.map(
+                                                          (val) {
+                                                            return DropdownMenuItem<
+                                                                String>(
+                                                              value: val,
+                                                              child: Text(val),
+                                                            );
+                                                          },
+                                                        ).toList(),
+                                                        onChanged:
+                                                            (String? val) {
+                                                          setState(
+                                                            () {
+                                                              _dropDownValue =
+                                                                  val;
+                                                            },
+                                                          );
+                                                        },
+                                                      ))),
+                                              Align(
+                                                alignment: Alignment.bottomLeft,
                                                 child: SizedBox(
-                                                    width: 100,
+                                                    width: 120,
                                                     height: 50,
                                                     child: ElevatedButton(
                                                         child: Text("Appoint"),
@@ -213,16 +154,17 @@ class _MainPageState extends State<MainPage> {
                                                                     color: Colors
                                                                         .black87))),
                                                         onPressed: () {
-                                                          selectedDoctor =
-                                                              document[
-                                                                  "doctorName"];
-                                                          selectedClinic =
-                                                              document[
-                                                                  "clinicName"];
-                                                          selectedPhone =
-                                                              document["phone"];
-                                                          _appointment(context);
-                                                          // _selectDate(context);
+                                                          FirebaseFirestore
+                                                              .instance
+                                                              .collection(
+                                                                  'appointment')
+                                                              .add({
+                                                            'created':
+                                                                _dropDownValue,
+                                                            'owner': uid
+                                                          });
+                                                          Navigator.pop(
+                                                              context, true);
                                                         })),
                                               )
                                             ],
@@ -232,26 +174,21 @@ class _MainPageState extends State<MainPage> {
                                 },
                                 child: Column(
                                   children: [
-                                    Icon(
-                                      Icons.add_moderator,
-                                      color: Colors.green,
-                                      size: 25,
-                                    ),
                                     ListTile(
-                                      title: Text(document["clinicName"]),
+                                      title: Text(document["name"]),
                                       subtitle: Text(document["type"]),
                                       // onTap: (){Navigator.push(cntext,MaterialPageRoute(builder: (context)=>HomePage()));},
                                     ),
-                                    RatingBarIndicator(
-                                      rating: document["rating"].toDouble(),
-                                      itemBuilder: (context, index) => Icon(
-                                        Icons.star,
-                                        color: Colors.amber,
-                                      ),
-                                      itemCount: 5,
-                                      itemSize: 20.0,
-                                      direction: Axis.horizontal,
-                                    ),
+                                    // RatingBarIndicator(
+                                    //   rating: document["rating"].toDouble(),
+                                    //   itemBuilder: (context, index) => Icon(
+                                    //     Icons.star,
+                                    //     color: Colors.amber,
+                                    //   ),
+                                    //   itemCount: 5,
+                                    //   itemSize: 20.0,
+                                    //   direction: Axis.horizontal,
+                                    // ),
                                   ],
                                 ),
                               )));
